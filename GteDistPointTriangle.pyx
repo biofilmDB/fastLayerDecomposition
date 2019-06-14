b2810eedc39da6a3dfff04439b4d03d3369da05f
Yotam Gingold <yotam@yotamgingold.com>
2019-06-14 14:43:44 -0400

    requirements.txt got frozen. runboth.sh script is more compatible. Added Cython language level.

--- GteDistPointTriangle.pyx
+++ GteDistPointTriangle.pyx
@@ -1,3 +1,4 @@
+# cython: language_level=2
 '''
 Ported from: http://www.geometrictools.com/GTEngine/Include/GteDistPointTriangle.h
 '''
--- image-layer-updating-GUI/runboth.sh
+++ image-layer-updating-GUI/runboth.sh
@@ -2,7 +2,9 @@
 
 ## Kill both with control-c
 ## From: https://superuser.com/questions/1118204/how-to-use-ctrlc-to-kill-all-background-processes-started-in-a-bash-script/1118226
-trap 'kill $BGPID; exit' SIGINT
+## Use INT not SIGINT
+## From: https://unix.stackexchange.com/questions/314554/why-do-i-get-an-error-message-when-trying-to-trap-a-sigint-signal
+trap 'kill $BGPID; exit' INT
 python3 server.py &
 BGPID=$!
 python3 -m http.server
