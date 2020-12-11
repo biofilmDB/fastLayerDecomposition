02692700e78c27658dbc29777b23e2e8b0604cbd
Lucy Williams <lgw2@uw.edu>
2020-12-11 10:17:19 -0700

    Restructure to have a main function

--- Our_preprocessing_pipeline.py
+++ Our_preprocessing_pipeline.py
@@ -19,7 +19,7 @@ Additive_mixing_layers_extraction.DEMO = True
 np.set_printoptions(precision=3, suppress=True)
 
 
-def get_snowcone_palette(pts):
+def get_snowcone_palette(pts, filepath):
     """Compute the snowcone palette from an existing palette choice."""
     M = len(pts)
     print("original palette is:")
@@ -219,82 +219,83 @@ def get_sphere_img():
     return points
 
 
-# Execute code
-# for testing, we'll define a sphere of points instead of reading an image from
-# file.
-from_file = True
+def main():
+    from_file = True
 
-base_dir = "./test/"
-if from_file:
-    filepath = glob.glob(base_dir + "*.png")[0]
+    base_dir = "./test/"
+    if from_file:
+        filepath = glob.glob(base_dir + "*.png")[0]
+        print(filepath)
+        img = np.asfarray(Image.open(filepath).convert('RGB'))/255.0
+    else:
+        filepath = base_dir + "sphere.png"
+        img = get_sphere_img()
+
+    print("#####################")
     print(filepath)
-    img = np.asfarray(Image.open(filepath).convert('RGB'))/255.0
-else:
-    filepath = base_dir + "sphere.png"
-    img = get_sphere_img()
+    print("img.shape is")
+    print(img.shape)
+    arr = img.copy()
+    X, Y = np.mgrid[0:img.shape[0], 0:img.shape[1]]
+    XY = np.dstack((X*1.0/img.shape[0], Y*1.0/img.shape[1]))
+    data = np.dstack((img, XY))
+    # data is a grid of 5d points: r, g, b, x, y
+    print("Size of RGBXY data set:")
+    print(len(data.reshape((-1, 5))))
+
+    # get palette
+
+    start = time.time()
+    palette_rgb = Additive_mixing_layers_extraction.\
+        Hull_Simplification_determined_version(
+            img,  # input data
+            filepath[:-4]+"-convexhull_vertices"  # filepath to write
+        )
+    palette_rgb = get_snowcone_palette(palette_rgb, filepath)
+    print("palette_rbg is")
+    print(palette_rgb)
+    end = time.time()
+    M = len(palette_rgb)
+    print("snowcone palette size: ", M)
+    print("palette extraction time: ", end-start)
+
+    palette_img = get_bigger_palette_to_show(palette_rgb)
+    Image.fromarray((palette_img*255).round().astype(np.uint8)).\
+        save(filepath[:-4]+"-convexhull_vertices.pdf")
+
+    # get layer decomposition
+
+    # for RGBXY RGB black star triangulation.
+    start = time.time()
+    data_hull = ConvexHull(data.reshape((-1, 5)))
+    start2 = time.time()
+    print("convexhull on 5D time: ", start2-start)
+    mixing_weights_1 = Additive_mixing_layers_extraction.\
+        Get_ASAP_weights_using_Tan_2016_triangulation_and_then_barycentric_coordinates( # noqa
+            img.reshape((-1, 3))[data_hull.vertices].reshape((-1, 1, 3)),
+            palette_rgb,
+            filepath[:-4],
+            order=0)
+    mixing_weights_2 = Additive_mixing_layers_extraction.\
+        recover_ASAP_weights_using_scipy_delaunay(
+            data_hull.points[data_hull.vertices],
+            data_hull.points, option=3)
+
+    mixing_weights = mixing_weights_2.dot(mixing_weights_1.reshape((-1, M)))
+    print("Mixing weights 2 shape:", mixing_weights_2.shape)
+    print("Mixing weights 1 shape:", mixing_weights_1.reshape((-1, M)).shape)
+
+    end = time.time()
+    print("total time: ", end-start)
+
+    mixing_weights = mixing_weights.reshape(
+        (img.shape[0], img.shape[1], -1)).clip(0, 1)
+
+    output_prefix = filepath[:-4]+'-RGBXY_RGB_black_star_ASAP'
+    print("palette rgb")
+    print(palette_rgb)
+    RMSE = save_weights(arr, palette_rgb, mixing_weights, output_prefix)  # noqa
 
 
-print("#####################")
-print(filepath)
-print("img.shape is")
-print(img.shape)
-arr = img.copy()
-X, Y = np.mgrid[0:img.shape[0], 0:img.shape[1]]
-XY = np.dstack((X*1.0/img.shape[0], Y*1.0/img.shape[1]))
-data = np.dstack((img, XY))
-# data is a grid of 5d points: r, g, b, x, y
-print("Size of RGBXY data set:")
-print(len(data.reshape((-1, 5))))
-
-# get palette
-
-start = time.time()
-palette_rgb = Additive_mixing_layers_extraction.\
-    Hull_Simplification_determined_version(
-        img,  # input data
-        filepath[:-4]+"-convexhull_vertices"  # filepath to write
-    )
-palette_rgb = get_snowcone_palette(palette_rgb)
-print("palette_rbg is")
-print(palette_rgb)
-end = time.time()
-M = len(palette_rgb)
-print("snowcone palette size: ", M)
-print("palette extraction time: ", end-start)
-
-palette_img = get_bigger_palette_to_show(palette_rgb)
-Image.fromarray((palette_img*255).round().astype(np.uint8)).\
-    save(filepath[:-4]+"-convexhull_vertices.pdf")
-
-# get layer decomposition
-
-# for RGBXY RGB black star triangulation.
-start = time.time()
-data_hull = ConvexHull(data.reshape((-1, 5)))
-start2 = time.time()
-print("convexhull on 5D time: ", start2-start)
-mixing_weights_1 = Additive_mixing_layers_extraction.\
-    Get_ASAP_weights_using_Tan_2016_triangulation_and_then_barycentric_coordinates( # noqa
-        img.reshape((-1, 3))[data_hull.vertices].reshape((-1, 1, 3)),
-        palette_rgb,
-        filepath[:-4],
-        order=0)
-mixing_weights_2 = Additive_mixing_layers_extraction.\
-    recover_ASAP_weights_using_scipy_delaunay(
-        data_hull.points[data_hull.vertices],
-        data_hull.points, option=3)
-
-mixing_weights = mixing_weights_2.dot(mixing_weights_1.reshape((-1, M)))
-print("Mixing weights 2 shape:", mixing_weights_2.shape)
-print("Mixing weights 1 shape:", mixing_weights_1.reshape((-1, M)).shape)
-
-end = time.time()
-print("total time: ", end-start)
-
-mixing_weights = mixing_weights.reshape(
-    (img.shape[0], img.shape[1], -1)).clip(0, 1)
-
-output_prefix = filepath[:-4]+'-RGBXY_RGB_black_star_ASAP'
-print("palette rgb")
-print(palette_rgb)
-RMSE = save_weights(arr, palette_rgb, mixing_weights, output_prefix)
+if __name__ == "__main__":
+    main()
